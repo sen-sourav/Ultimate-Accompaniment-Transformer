2bc2164eb2786436e811482d4baee9f38036c469
sen-sourav <souravsen1990@gmail.com>
2024-08-07 05:30:51 -0500

    corrected directory structure

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ setup(
     version="0.1",
     packages=find_packages(),
     install_requires=[
-        # List your dependencies here
         'torch',
         'tqdm',
         'matplotlib',
@@ -15,4 +14,3 @@ setup(
         # Add any other dependencies
     ],
 )
-
